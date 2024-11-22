343c7560da4c8d03b719946a51fc608d9bd229c3
DzikStar <dawid0mila@gmail.com>
2024-11-22 23:36:02 +0100

    website v3

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,42 @@
-alert("This website is currently WIP. However you can still access it ;)")
+function pageUnderConstruction(){
+    const pageMount = document.getElementById("page-mount");
+
+    const underConstruction = document.createElement("div");
+    const content= document.createElement("p")
+
+    content.textContent = `The maze isn't meant for you`;
+
+    underConstruction.appendChild(content)
+    pageMount.appendChild(underConstruction);
+}
+
+function buildList(kind){
+    const pageMount = document.getElementById("page-mount");
+
+    const listContainer = document.createElement("div");
+    listContainer.className = "experimentsListContainer";
+
+    const ul = document.createElement("ul");
+
+    for (let i = 1; i <= 11; i++) {
+        const li = document.createElement("li");
+        li.className = "experimentsListItem";
+        li.textContent = `${kind}_${i}`;
+        li.addEventListener("click", function(){
+            openExperiment(li.textContent)
+        })
+        ul.appendChild(li);
+    }
+
+    listContainer.appendChild(ul);
+
+    pageMount.appendChild(listContainer);
+}
+
+function openExperiment(exp_id){
+    alert(exp_id)
+    /*
+        TODO
+    */
+   return;
+}
