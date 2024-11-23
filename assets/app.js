5d3b2d918b842ffe0a231bde763d17bc85ed4a07
DzikStar <82441193+DzikStar@users.noreply.github.com>
2024-11-24 00:34:54 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,38 +1,23 @@
-function pageUnderConstruction() {
-  const pageMount = document.getElementById("page-mount");
+function pageUnderConstruction(){
+    const pageMount = document.getElementById("page-mount");
 
-  const underConstruction = document.createElement("div");
-  underConstruction.style.backgroundColor = "#f0f2f5";
-  underConstruction.style.border = "1px solid #ccc";
-  underConstruction.style.borderRadius = "5px";
-  underConstruction.style.padding = "20px";
-  underConstruction.style.fontFamily = "sans-serif";
-  underConstruction.style.margin = "20px";
-  underConstruction.style.width = "400px"; // Adjust width as needed
+    const underConstruction = document.createElement("div");
+    const header = document.createElement("h1");
+    const description = document.createElement("p");
 
-  const cardHeader = document.createElement("div");
-  cardHeader.style.backgroundColor = "#232629";
-  cardHeader.style.color = "#fff";
-  cardHeader.style.padding = "10px";
-  cardHeader.style.borderRadius = "5px 5px 0 0";
-  cardHeader.style.width = "100%";
+    underConstruction.style.paddingLeft = "20px";
+    underConstruction.style.paddingTop = "20px";
 
-  const header = document.createElement("h1");
-  header.textContent = "The maze isn't meant for you";
-  header.style.textAlign = "center";
+    header.style.margin = 0;
+    header.textContent = `The maze isn't meant for you`;
 
-  cardHeader.appendChild(header);
+    description.textContent = "Service Work In Progress - come back later!"
 
-  const description = document.createElement("p");
-  description.textContent = "Service Work In Progress - come back... someday?";
-  description.style.color = "#66757c";
+    underConstruction.appendChild(header);
+    underConstruction.appendChild(description);
 
-  underConstruction.appendChild(cardHeader);
-  underConstruction.appendChild(description);
-
-  pageMount.appendChild(underConstruction);
+    pageMount.appendChild(underConstruction);
 }
-
 
 async function buildList(kind) {
     const pageMount = document.getElementById("page-mount");
