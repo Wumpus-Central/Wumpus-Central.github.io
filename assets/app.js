40d2011e3338f29aa316da7fd7172fb17370ab0d
DzikStar <dawid0mila@gmail.com>
2024-11-23 16:15:13 +0100

    experiments list implemented

--- assets/app.js
+++ assets/app.js
@@ -22,19 +22,26 @@ function pageUnderConstruction(){
 function buildList(kind){
     const pageMount = document.getElementById("page-mount");
 
+    let experiments = fetch('../experiments-archive/data/experiments.json').then(res => res.json());
+    experiments.reverse();
+
     const listContainer = document.createElement("div");
     listContainer.className = "experimentsListContainer";
 
     const ul = document.createElement("ul");
 
-    for (let i = 1; i <= 11; i++) {
-        const li = document.createElement("li");
-        li.className = "experimentsListItem";
-        li.textContent = `${kind}_${i}`;
-        li.addEventListener("click", function(){
-            openExperiment(li.textContent);
-        })
-        ul.appendChild(li);
+    for (let i = 0; i < experiments.length; i++) {
+        const experiment = experiments[i];
+        
+        if (experiment.kind === kind) {
+            const li = document.createElement("li");
+            li.className = "experimentsListItem";
+            li.textContent = experiment.id;
+            li.addEventListener("click", function(){
+                openExperiment(li.textContent);
+            })
+            ul.appendChild(li);
+        }
     }
 
     listContainer.appendChild(ul);
